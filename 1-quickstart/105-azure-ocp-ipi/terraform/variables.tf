306ec0819d20ec9bb78f2230ce5e6bef7d9e8a25
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-05-25 13:18:23 +1000

    Initial draft
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

---
+++ 1-quickstart/105-azure-ocp-ipi/terraform/variables.tf
@@ -0,0 +1,44 @@
+variable "resource_group_name" {
+  type = string
+  description = "The name of the resource group that contains the public DNS zone"
+}
+
+variable "openshift_version" {
+  type = string
+  description = "Version of Openshift to install"
+}
+
+variable "region" {
+  type = string
+  description = "The Azure location where the resource group will be provisioned"
+}
+variable "subscription_id" {
+  type = string
+  description = "the value of subscription_id"
+}
+variable "client_id" {
+  type = string
+  description = "the value of client_id"
+}
+variable "client_secret" {
+  type = string
+  description = "the value of client_secret"
+}
+variable "tenant_id" {
+  type = string
+  description = "the value of tenant_id"
+}
+
+variable "cluster_name" {
+  type = string
+  description = "The name of the cluster to create"
+  default = ""
+}
+
+variable "base_domain_name" {
+  type = string
+}
+
+variable "pull_secret" {
+  type = string
+}
