7240b82a62a15d5247493b77e9c2d2e258d6bed7
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-12-02 15:32:52 +1100

    Adds support for macOS
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- launch.sh
+++ launch.sh
@@ -3,13 +3,15 @@
 # IBM Ecosystem Labs
 
 SCRIPT_DIR="$(cd $(dirname $0); pwd -P)"
-SRC_DIR="${SCRIPT_DIR}"
-STOP_FILE="${SCRIPT_DIR}/.stop"
+SRC_DIR="${SCRIPT_DIR}/automation"
+AUTOMATION_BASE=$(basename "${SCRIPT_DIR}")
+STOP_FILE="${SCRIPT_DIR}/.stop"     # This is only used for flow control between the container and this script
+PROVIDER_DNS="8.8.8.8"    # This is used to override local DNS when using Colima on MacOS
 
-DOCKER_IMAGE="quay.io/cloudnativetoolkit/cli-tools-azure:v1.2-v0.6.0"
+DOCKER_IMAGE="quay.io/cloudnativetoolkit/cli-tools-azure:v1.2-v0.6.1"
 
 SUFFIX=$(echo $(basename ${SCRIPT_DIR}) | base64 | sed -E "s/[^a-zA-Z0-9_.-]//g" | sed -E "s/.*(.{5})/\1/g")
-CONTAINER_NAME="cli-tools-${SUFFIX}"
+CONTAINER_NAME="azure-${SUFFIX}"
 
 echo "Cleaning up old container: ${CONTAINER_NAME}"
 
@@ -19,6 +21,38 @@ if [[ -e $STOP_FILE ]]; then
 fi
 
 DOCKER_CMD="docker"
+if [[ -n "$1" ]] && [[ "$1" != "--pull" ]]; then
+  DOCKER_CMD="${1:-docker}"
+fi
+
+if [[ ! -d "${SRC_DIR}" ]]; then
+  SRC_DIR="${SCRIPT_DIR}"
+fi
+
+# check if colima is installed, and apply dns override if no override file already exists
+if command -v colima &> /dev/null
+then
+  if [ ! -f ~/.lima/_config/override.yaml ] || [ -z "$(cat ~/.lima/_config/override.yaml | grep "$PROVIDER_DNS" 2> /dev/null )" ]; then
+    echo "applying colima dns override for $PROVIDER_DNS..."
+
+    COLIMA_STATUS="$(colima status 2>&1)"
+    SUB='colima is running'
+    if [[ "$COLIMA_STATUS" == *"$SUB"* ]]; then
+      echo "stopping colima"
+      colima stop
+    fi
+
+    echo "writing ~/.lima/_config/override.yaml"
+    mkdir -p ~/.lima/_config
+    printf "useHostResolver: false\ndns:\n- $PROVIDER_DNS" > ~/.lima/_config/override.yaml
+
+    if [[ "$COLIMA_STATUS" == *"$SUB"* ]]; then
+      echo "restarting colima"
+      colima start
+    fi
+  fi
+fi
+
 ${DOCKER_CMD} kill ${CONTAINER_NAME} 1> /dev/null 2> /dev/null
 ${DOCKER_CMD} rm ${CONTAINER_NAME} 1> /dev/null 2> /dev/null
 
@@ -36,16 +70,16 @@ echo -n "Setup workspace (y/n) [Y]: "
 read SETUP
 echo
 
-  echo "Initializing container ${CONTAINER_NAME} from ${DOCKER_IMAGE} and setting up environment"
-  ${DOCKER_CMD} run -itd --name ${CONTAINER_NAME} \
-    --device /dev/net/tun --cap-add=NET_ADMIN \
-    -v ${SRC_DIR}:/terraform \
-    -v workspace:/workspaces \
-    -w /terraform \
-    ${ENV_FILE} \
-    ${DOCKER_IMAGE}
+echo "Initializing container ${CONTAINER_NAME} from ${DOCKER_IMAGE} and setting up environment"
+${DOCKER_CMD} run -itd --name ${CONTAINER_NAME} \
+  --device /dev/net/tun --cap-add=NET_ADMIN \
+  -v ${SRC_DIR}:/terraform \
+  -v workspace-${AUTOMATION_BASE}-${UID}:/workspaces \
+  -w /terraform \
+  ${ENV_FILE} \
+  ${DOCKER_IMAGE}
 
-if [[ "${SETUP^}" == "Y" ]] || [[ -z $SETUP ]]; then
+if [[ "$(echo $SETUP | tr '[:lower:]' '[:upper:]' )" == "Y" ]] || [[ -z $SETUP ]]; then
 
     # Check if service principal details provided, if not login to Azure CLI
     echo "Checking credentials & logging into Azure CLI"
@@ -66,7 +100,7 @@ if [[ "${SETUP^}" == "Y" ]] || [[ -z $SETUP ]]; then
     echo -n "Build environment (only yes will be accepted) : "
     read BUILD
 
-    if [[ "${BUILD^^}" == "YES" ]]; then
+    if [[ "$(echo $BUILD | tr '[:lower:]' '[:upper:]' )" == "YES" ]]; then
       ${DOCKER_CMD} exec -it -w /workspaces/current ${CONTAINER_NAME} sh -c "./apply-all.sh -a"
     else
       echo
