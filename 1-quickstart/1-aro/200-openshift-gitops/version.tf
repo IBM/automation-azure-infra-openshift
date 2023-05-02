f268b13acb54a6f0476e7459aa74aff4daa1c565
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2023-05-02 13:40:08 +1000

    Updates gitops to latest for ARO QS
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- 1-quickstart/1-aro/200-openshift-gitops/version.tf
+++ 1-quickstart/1-aro/200-openshift-gitops/version.tf
@@ -1,13 +1,11 @@
 terraform {
   required_providers {
-    gitops = {
-      source = "cloud-native-toolkit/gitops"
-      version = "0.11.1"
-    }
-
     clis = {
       source = "cloud-native-toolkit/clis"
-      version = "0.2.4"
+    }
+
+    gitops = {
+      source = "cloud-native-toolkit/gitops"
     }
 
   }
